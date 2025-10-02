73adcb4da396126a8c50df6c6267719b24317712
Nguyen Van Hien Vinh <clytusnguyen@gmail.com>
2025-10-02 17:15:19 +0700

    Update

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import ui.LoginUI;
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new LoginUI().setVisible(true);
+        });
+    }
+}
