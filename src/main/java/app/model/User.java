46b9b2c0289449c565e7f617805b0551f03318ae
ylc3205 <clytusnguyen@gmail.com>
2025-10-29 21:18:29 +0700

    abstract and poly

--- src/main/java/app/model/User.java
+++ src/main/java/app/model/User.java
@@ -1,10 +1,11 @@
 package app.model;
 
-public class User {
-    private String userId;
-    private String name;
-    private String email;
-    private String password;
+// abstract class
+public abstract class User {
+    protected String userId;
+    protected String name;
+    protected String email;
+    protected String password;
 
     public User(String userId, String name, String email, String password) {
         this.userId = userId;
@@ -22,4 +23,34 @@ public class User {
     public void setName(String name) { this.name = name; }
     public void setEmail(String email) { this.email = email; }
     public void setPassword(String password) { this.password = password; }
+
+    public abstract String getRole();
+
+    public void showInfo() {
+        System.out.println("ID: " + userId + ", Name: " + name + ", Role: " + getRole());
+    }
+}
+
+// AdminUser
+class AdminUser extends User {
+    public AdminUser(String userId, String name, String email, String password) {
+        super(userId, name, email, password);
+    }
+
+    @Override
+    public String getRole() {
+        return "Administrator";
+    }
+}
+
+// NormalUser
+class NormalUser extends User {
+    public NormalUser(String userId, String name, String email, String password) {
+        super(userId, name, email, password);
+    }
+
+    @Override
+    public String getRole() {
+        return "Regular User";
+    }
 }
